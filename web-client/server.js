2f2140ac7cd7472d9f6680ee568e82d9dd17361d
KingaPiasecka <piasecka.kingaa@gmail.com>
2018-09-02 19:37:56 +0200

    proxy setting

--- web-client/server.js
+++ web-client/server.js
@@ -9,11 +9,20 @@ const request = require('request');
 
 app.use(cors());
 cors({credentials: true, origin: true});
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+
+
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'example.com');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+};
+
+app.configure(function() {
+  app.use(allowCrossDomain);
+});
+
 
 app.get('/api/login',cors(),  function(req, res) {
   req.get({url: 'clasiixer-server.herokuapp.com/login', headers: req.headers});
