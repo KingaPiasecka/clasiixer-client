bfffe397ec8e058b3f7fd2faac93c83f566b75bb
KingaPiasecka <piasecka.kingaa@gmail.com>
2018-09-02 23:02:01 +0200

    url

--- web-client/server.js
+++ web-client/server.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
 });
 
 
-app.all('/*', function(req, res, next) {
+app.all('/api/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.url = 'clasiixer-server.herokuapp.com';
