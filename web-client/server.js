9b03931ece2f0bb2ab15c40601d7e2594c0e8ca4
KingaPiasecka <piasecka.kingaa@gmail.com>
2018-09-02 17:44:52 +0200

    proxy setting

--- web-client/server.js
+++ web-client/server.js
@@ -4,19 +4,24 @@ const path = require('path');
 const proxy = require('express-http-proxy');
 const app = express();
 
+const request = require('request');
+
+router.get('/api', function(req, res){
+
+  request('https://clasiixer-server.herokuapp.com', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred and handle it
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    res.send(body)
+  });
+
+});
+
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/web-client'));
-
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
   res.sendFile(path.join(__dirname+ '/dist/web-client/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-const globalTunnel = require('global-tunnel');
-
-globalTunnel.initialize({
-  host: 'https://clasiixer-server.herokuapp.com'
-});
