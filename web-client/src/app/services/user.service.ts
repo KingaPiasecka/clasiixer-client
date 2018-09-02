d26524dc739b074b55d3c1b21edde59693ffda0e
KingaPiasecka <piasecka.kingaa@gmail.com>
2018-09-02 03:01:20 +0200

    server url

--- web-client/src/app/services/user.service.ts
+++ web-client/src/app/services/user.service.ts
@@ -13,11 +13,11 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   register(user: User): Observable<TextResponse> {
-    return this.http.post<TextResponse>('https://clasiixer-client.herokuapp.com/register', user);
+    return this.http.post<TextResponse>('clasiixer-client.herokuapp.com/register', user);
   }
 
   getUsername(): Observable<Username> {
-    return this.http.get<Username>('https://clasiixer-client.herokuapp.com/currentUser');
+    return this.http.get<Username>('clasiixer-client.herokuapp.com/currentUser');
   }
 
 }
