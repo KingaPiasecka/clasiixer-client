6c19b5e195b09d471a82a477ebedab0d457563e4
KingaPiasecka <piasecka.kingaa@gmail.com>
2018-09-02 19:16:34 +0200

    proxy setting

--- web-client/server.js
+++ web-client/server.js
@@ -6,6 +6,7 @@ const app = express();
 const router = express.Router();
 const cors = require('cors');
 const request = require('request');
+app.options('*', cors());
 app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
